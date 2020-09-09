084b7c12c7237e85d5fe3ae4b9835104b6ca6f58
prodevengineer <64164769+prodevengineer@users.noreply.github.com>
2020-09-08 19:02:22 -0700

    Add files via upload

---
+++ Module 3/3.1/public/scripts.js
@@ -0,0 +1,27 @@
+const modalOverlay=document.querySelector('.modal-overlay')
+const cards= document.querySelectorAll('.card')
+const modal=document.querySelector('.modal')
+
+for (let card of cards){
+  card.addEventListener("click",function(){ 
+    modalOverlay.classList.add('active')
+    
+    // modal.classList.add('maximize')
+
+    const courseId= card.getAttribute("id")
+    modalOverlay.querySelector("iframe").src=`https:rocketseat.com.br/${courseId}`
+    
+    if (modal.classList.contains('maximize')){
+      modal.classList.remove('maximize')
+    } else {
+      modal.classList.add('maximize')
+    }
+
+  })
+}
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+  modalOverlay.classList.remove('active')
+})
+
+
